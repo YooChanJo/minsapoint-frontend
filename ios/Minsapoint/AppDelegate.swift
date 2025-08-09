9f811c1ad373cc333bdd2112670d3d70067d6af6
Yoo-Chan Jo <jo.yoochani@gmail.com>
2025-08-09 15:21:15 +0900

    Temporary commit, added linking, firebase features for ios, yet get a build error --> Needs fixing

--- ios/Minsapoint/AppDelegate.swift
+++ ios/Minsapoint/AppDelegate.swift
@@ -2,35 +2,55 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
+// firebase
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
-
+  
   var reactNativeDelegate: ReactNativeDelegate?
   var reactNativeFactory: RCTReactNativeFactory?
-
+  
   func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
+    // firebase
+    FirebaseApp.configure()
+    
     let delegate = ReactNativeDelegate()
     let factory = RCTReactNativeFactory(delegate: delegate)
     delegate.dependencyProvider = RCTAppDependencyProvider()
-
+    
     reactNativeDelegate = delegate
     reactNativeFactory = factory
-
+    
     window = UIWindow(frame: UIScreen.main.bounds)
-
+    
     factory.startReactNative(
       withModuleName: "Minsapoint",
       in: window,
       launchOptions: launchOptions
     )
-
+    
     return true
   }
+  // enable linking
+  override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+    return RCTLinkingManager.application(app, open: url, options: options)
+  }
+  // universal links
+  override func application(
+    _ application: UIApplication,
+    continue userActivity: NSUserActivity,
+    restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+      return RCTLinkingManager.application(
+        application,
+        continue: userActivity,
+        restorationHandler: restorationHandler
+      )
+    }
 }
 
 class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
